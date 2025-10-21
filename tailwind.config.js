02f6878535ef764a84ae21db7ffdfd765cde0c15
moaz tello <tello.m.m.t.95@gmail.com>
2025-10-21 07:34:45 +0300

    third

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        reem: ["'Reem Kufi', sans-serif"],
         kufam: ["'Kufam', sans-serif"],
-        amiri: ["'Amiri', serif"],
       },
     },
   },
